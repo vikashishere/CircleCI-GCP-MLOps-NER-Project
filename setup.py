17b6084cfbe8d1b5651bd6d46f5005d2d9081c0d
Vikash <vikashdas770@gmail.com>
2024-04-25 10:06:23 +0530

    venv setup and folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="Vikash Das",
+    author_email="vikashdas770@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
