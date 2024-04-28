1cba913c7dd1a12716d8be968c02eb524cdab7d8
Vikash <vikashdas770@gmail.com>
2024-04-28 10:56:45 +0530

    Revert "Data Ingestion component added"
    This reverts commit 5775a591b82c68ae328c3509e32b56f02343f75b.

--- demo.py
+++ demo.py
@@ -1,25 +1,4 @@
-# # Demo on downloading data from gcloud storage
+from ner.configuration.gcloud import GCloud
 
-# from ner.configuration.gcloud import GCloud
-
-# obj = GCloud()
-# obj.sync_folder_from_gcloud(gcp_bucket_url="ner-using-bert-vikash", filename="archive.zip", destination="gcp_download")
-
-# -------------------------------------------------------------------------------------------------------------------------
-
-# Demo on Data Ingestion Component
-
-import os, sys
-from ner.exception import NerException
-from ner.pipeline.train_pipeline import TrainPipeline
-from ner.constants import *
-
-def training():
-    try:
-        train_pipeline = TrainPipeline()
-        train_pipeline.run_pipeline()
-    except Exception as e:
-        raise NerException(e, sys) from e
-    
-if __name__=="__main__":
-    training()
+obj = GCloud()
+obj.sync_folder_from_gcloud(gcp_bucket_url="ner-using-bert-vikash", filename="archive.zip", destination="gcp_download")
--- ner/entity/artifact_entity.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-# Data Ingestion Artifacts
-@dataclass
-class DataIngestionArtifacts:
-    zip_data_file_path: str
-    csv_data_file_path: str
